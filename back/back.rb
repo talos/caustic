1c23e7728de0aea9aa9f0f8a7dc3c7513d47213b
talos <irving.krauss@gmail.com>
2010-11-27 15:51:55 -0500

    minor updates to back.rb

--- back/back.rb
+++ back/back.rb
@@ -374,7 +374,7 @@ post '/type/:parent/child/:child' do
   @child = Type.first(:name => params[:child])
   @parent = Type.first(:name => params[:parent])
   @child.parent = @parent
-  @child.save ? true : {:child => @child.errors.to_a, :parent => @parent.errors.to_a}.to_json
+  @child.save ? @child.to_json : {:child => @child.errors.to_a, :parent => @parent.errors.to_a}.to_json
 end
 
 # Remove a Type's child relationship.  Does not eliminate either the child or parent Type.
@@ -382,7 +382,7 @@ end
 delete '/type/:parent/child/:child' do
   @parent = Type.first(:name => params[:parent])
   @parent.children.first(:name => params[:child]).delete
-  @parent.save ? true : {:parent => @parent.errors.to_a}.to_json
+  @parent.save ? @parent.to_json : {:parent => @parent.errors.to_a}.to_json
 end
 
 # Add a publishField to a type.
@@ -390,14 +390,14 @@ post '/type/:type/publish/:publish_field' do
   @type = Type.first(:name => params[:type])
   @publish_field = PublishField.new(:name => params[:publish_field])
   @type.publish_fields << @publish_field
-  @type.save ? true : {:type => @type.errors.to_a, :publish_field => @publish_field.errors.to_a}.to_json
+  @type.save ? @type.to_json : {:type => @type.errors.to_a, :publish_field => @publish_field.errors.to_a}.to_json
 end
 
 # Delete a publishField from a type.
 delete '/type/:type/publish/:publish_field' do
   @type = Type.first(:name => params[:type])
   @type.publish_fields.first(:name => params[:publish_field]).delete
-  @type.save ? true : {:type => @type.errors.to_a}.to_json
+  @type.save ? @type.to_json : {:type => @type.errors.to_a}.to_json
 end
 
 # Create a new Information.
@@ -409,14 +409,14 @@ post '/information/:namespace/:type' do
   @namespace.informations << @information
   @type.informations << @information
 
-  @information.save ? true : {:information => @information.errors.to_a, :namespace => @namespace.errors.to_a, :type => @type}.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a, :namespace => @namespace.errors.to_a, :type => @type}.to_json
 end
 
 # Delete an Information.
 delete '/information/:namespace/:type' do
   @information = Information.first(:namespace_name => params[:namespace], :type_name => params[:type])
   @information.delete
-  @information.save ? true : {:information => @information.errors.to_a}.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a}.to_json
 end
 
 # Add a gatherer to an Information.
@@ -425,7 +425,7 @@ post '/information/:namespace/:type/gatherer/:gatherer' do
   @gatherer = Gatherer.first(:name => params[:gatherer])
 
   @information.gatherers << @gatherer
-  @information.save ? true : {:information => @information.errors.to_a, :gatherer => @gatherer.errors.to_a}.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a, :gatherer => @gatherer.errors.to_a}.to_json
 end
 
 # Add a DefaultField to an Information.
@@ -433,14 +433,14 @@ post '/information/:namespace/:type/default/:name' do
   @information = Information.first(:namespace_name => params[:namespace], :type_name => params[:type])
   @default_field = DefaultField.new(:name => params[:name], :value => params[:value])
   @information.default_fields << @defaultField
-  @information.save ? true : {:information => @information.errors.to_a, :default_field => @default_field.errors.to_a }.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a, :default_field => @default_field.errors.to_a }.to_json
 end
 
 # Delete a DefaultField from an Information.
 delete '/information/:namespace/:type/default/:name' do
   @information = Information.first(:namespace_name => params[:namespace], :type_name => params[:type])
   @information.default_fields.first(:name => params[:name]).delete
-  @information.save ? true : {:information => @information.errors.to_a }.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a }.to_json
 end
 
 # Add a ToField to an Information.  Regex is inside the post.
@@ -448,14 +448,14 @@ post '/information/:namespace/:type/:input_field/:match_number/to/:destination_f
   @information = Information.first(:namespace_name => params[:namespace], :type_name => params[:type])
   @to_field = ToField.new(:input_field => params[:input_field], :match_number => params[:match_number], :regex => params[:regex], :destination_field => params[:destination_field])
   @information.to_fields << @to_field
-  @information.save ? true : {:information => @information.errors.to_a, :to_field => @to_field.errors.to_a}.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a, :to_field => @to_field.errors.to_a}.to_json
 end
 
 # Delete a ToField from an Information
 delete '/information/:namespace/:type/:input_field/:match_number/to/:destination_field' do
   @information = Information.first(:namespace_name => params[:namespace], :type_name => params[:type])
   @information.to_fields.first(:input_field => params[:input_field], :match_number => params[:match_number], :destination_field => params[:destination_field]).delete
-  @information.save ? true : {:information => @information.errors.to_a }.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a }.to_json
 end
 
 # Add a ToInformation to an Information.
@@ -464,7 +464,7 @@ post '/information/:namespace/:type/:input_field/to/:destination_namespace/:dest
   @destination_information = Information.first(:namespace_name => params[:destination_namespace], :type_name => params[:destination_type])
   @to_information = ToInformation.new(:input_field => params[:input_field], :regex => params[:regex], :destination_information => @destination_information, :destination_field => params[:destination_field])
   @information.to_informations << @to_information
-  @information.save ? true : {:information => @information.errors.to_a, :destination_information => @destination_information.errors.to_a, :to_information => @to_information.errors.to_a}.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a, :destination_information => @destination_information.errors.to_a, :to_information => @to_information.errors.to_a}.to_json
 end
 
 # Delete a ToInformation from an Information.
@@ -472,27 +472,27 @@ delete '/information/:namespace/:type/:input_field/to/:destination_namespace/:de
   @information = Information.first(:namespace_name => params[:namespace], :type_name => params[:type])
   @destination_information = Information.first(:namespace_name => params[:destination_namespace], :type => params[:destination_type])
   @information.to_informations.first(:input_field => params[:input_field], :destination_information => @destination_information,  :destination_field => params[:destination_field]).delete
-  @information.save ? true: {:information => @information.errors.to_a, :destination_information => @destination_information.errors.to_a}.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a, :destination_information => @destination_information.errors.to_a}.to_json
 end
 
 # Delete a Gatherer from an Information.  Does not eliminate the Gatherer itself.
 delete '/information/:namespace/:type/gatherer/:gatherer' do
   @information = Information.first(:namespace_name => params[:namespace], :type_name => params[:type])
   @information.gatherers.first(:name => params[:gatherer]).delete
-  @information.save ? true: {:information => @information.errors.to_a }.to_json
+  @information.save ? @information.to_json : {:information => @information.errors.to_a }.to_json
 end
 
 # Create a new Gatherer.
 post '/gatherer/:name' do
   @gatherer = Gatherer.new(:name => params[:name])
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a}.to_json
 end
 
 # Delete a Gatherer.
 delete '/gatherer/:name' do
   @gatherer = Gatherer.first(:name => params[:name])
   @gatherer.delete
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a}.to_json
 end
 
 # Add a parent to a Gatherer.
@@ -500,14 +500,14 @@ post '/gatherer/:child/parent/:parent' do
   @child =  Gatherer.first(:name => params[:child])
   @parent = Gatherer.first(:name => params[:parent])
   @child.parent = @parent
-  @child.save ? true : {:parent => @parent.errors.to_a, :child => @child.errors.to_a }.to_json
+  @child.save ? @child.to_json : {:parent => @parent.errors.to_a, :child => @child.errors.to_a }.to_json
 end
 
 # Remove a parent relationship from a Gatherer. Does not eliminate the parent.
 delete '/gatherer/:child/parent' do
   @child =  Gatherer.first(:name => params[:child])
   @child.parent.delete
-  @child.save ? true : {:child => @child.errors.to_a}.to_json
+  @child.save ? @child.to_json : {:child => @child.errors.to_a}.to_json
 end
 
 # Add a child to a Gatherer.
@@ -515,7 +515,7 @@ post '/gatherer/:parent/child/:child' do
   @child =  Gatherer.first(:name => params[:child])
   @parent = Gatherer.first(:name => params[:parent])
   @child.parent = @parent
-  @child.save ? true : {:child => @child.errors.to_a, :parent => @parent.errors.to_a}.to_json
+  @child.save ? @child.to_json : {:child => @child.errors.to_a, :parent => @parent.errors.to_a}.to_json
 end
 
 # Remove a child relationship from a Gatherer. Does not eliminate the child.
@@ -523,77 +523,77 @@ end
 delete '/gatherer/:parent/child/:child' do
   @parent = Gatherer.first(:name => params[:parent])
   @parent.children.first(:name => params[:child]).delete
-  @parent.save ? true : {:parent => @parent.errors.to_a}.to_json
+  @parent.save ? @parent.to_json : {:parent => @parent.errors.to_a}.to_json
 end
 
 # Add a URL to a Gatherer.  Value is in the post data.
 post '/gatherer/:gatherer/url' do
   @gatherer = Gatherer.first(:name => params[:gatherer])
   @url = @gatherer.urls.new(:value => params[:value])
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a, :url => @url.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a, :url => @url.errors.to_a}.to_json
 end
 
 # Delete a URL from a gatherer.
 delete '/gatherer/:gatherer/url/:value' do
   @gatherer = Gatherer.first(:name => params[:gatherer])
   @gatherer.urls.first(:value => params[:value]).delete
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a}.to_json
 end
 
 # Add a GET to a Gatherer.  Value is in the post data.
 post '/gatherer/:gatherer/get/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @get = @gatherer.gets.new(:name => params[:name], :value => params[:value])
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a, :get => @get.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a, :get => @get.errors.to_a}.to_json
 end
 
 # Delete a GET from a Gatherer.
 delete '/gatherer/:gatherer/get/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @gatherer.gets.first(:name => params[:name]).delete
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a}.to_json
 end
 
 # Add a POST to a Gatherer.  Value is in the post data.
 post '/gatherer/:gatherer/post/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @post = @gatherer.posts.new(:name => params[:name], :value => params[:value])
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a, :post => @post.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a, :post => @post.errors.to_a}.to_json
 end
 
 # Delete a POST from a Gatherer.
 delete '/gatherer/:gatherer/post/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @gatherer.posts.first(:name => params[:name]).delete
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a}.to_json
 end
 
 # Add a Header to a Gatherer.  Value is in the post data.
 post '/gatherer/:gatherer/header/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @header = @gatherer.headers.new(:name => params[:name], :value => params[:value])
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a, :header => @header.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a, :header => @header.errors.to_a}.to_json
 end
 
 # Delete a Header from a Gatherer.
 delete '/gatherer/:gatherer/header/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @gatherer.headers.first(:name => params[:name]).delete
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a}.to_json
-end
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a}.to_json
+end 
 
 # Add a Cookie to a Gatherer. Value is in the post data.
 post '/gatherer/:gatherer/cookie/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @cookie = @gatherer.cookies.new(:name => params[:name], :value => params[:value])
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a, :cookie =>  @cookie.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a, :cookie =>  @cookie.errors.to_a}.to_json
 end
 
 # Delete a Cookie from a Gatherer.
 delete '/gatherer/:gatherer/cookie/:name' do
   @gatherer = Gatherer.first(params[:gatherer])
   @gatherer.cookies.first(:name => params[:name]).delete
-  @gatherer.save ? true : {:gatherer => @gatherer.errors.to_a}.to_json
+  @gatherer.save ? @gatherer.to_json : {:gatherer => @gatherer.errors.to_a}.to_json
 end
 
 error do
--- back/tests/test.sh
+++ back/tests/test.sh
@@ -2,164 +2,87 @@
 
 URL="http://localhost:4567"
 
-echo 
+echo "Creating namespaces and types:"
 curl -X POST $URL/namespace/C36047
-echo
 curl -X POST $URL/namespace/NY.NYC.Brooklyn
-echo
 curl -X POST $URL/namespace/NY.NYC
-echo
 curl -X POST $URL/namespace/C36047/parent/NY.NYC.Brooklyn
-echo
 curl -X POST $URL/namespace/NY.NYC.Brooklyn/parent/NY.NYC
-echo
 curl -X POST $URL/type/Property
-echo
 curl -X POST $URL/type/Property/publish/streetNum
-echo
 curl -X POST $URL/type/Property/publish/streetDir
-echo
 curl -X POST $URL/type/Property/publish/streetName
-echo
 curl -X POST $URL/type/Property/publish/streetSuffix
-echo
 curl -X POST $URL/type/Property/publish/city
-echo
 curl -X POST $URL/type/Property/publish/zip
-echo
 curl -X POST $URL/type/Party
-echo
 curl -X POST $URL/type/Party/publish/name
-echo
 curl -X POST $URL/type/Party/publish/address
-echo
 curl -X POST $URL/information/NY.NYC.Brooklyn/Property
-echo
 curl -X POST -d "value=3" $URL/information/NY.NYC.Brooklyn/Property/default/boroughNumber
-echo
 
-echo
+echo "Creating gatherers:"
 curl -X POST $URL/gatherer/NY.NYC.BIS.Profile
-echo
 curl -X POST -d "value=http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet" $URL/gatherer/NY.NYC.BIS.Profile/url
-echo
 curl -X POST $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch
-echo
 curl -X POST -d "value=http://webapps.nyc.gov:8084/CICS/FIN1/FIND001I" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/url
-echo
 curl -X POST $URL/gatherer/NY.NYC.ACRIS.Parcel.All
-echo
 curl -X POST -d "value=http://a836-acris.nyc.gov/Scripts/DocSearch.dll/BBLResult"  $URL/gatherer/NY.NYC.ACRIS.Parcel.All/url
-echo
 curl -X POST $URL/gatherer/NY.NYC.ACRIS.Parcel.Deeds
-echo
 curl -X POST $URL/gatherer/NY.NYC.ACRIS.Parcel.Deeds/parent/NY.NYC.ACRIS.Parcel.All
-echo
 curl -X POST $URL/gatherer/NY.NYC.ACRIS.Parcel.Mortgages
-echo
 curl -X POST $URL/gatherer/NY.NYC.ACRIS.Parcel.Mortgages/parent/NY.NYC.ACRIS.Parcel.All
-echo
 curl -X POST -d "value=$R{boroughNumber}" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_borough
-echo
 curl -X POST -d "value=$R{block}" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_block
-echo
 curl -X POST -d "value=$R{lot}" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_lot
-echo
 curl -X POST -d "value=http://a836-acris.nyc.gov/Scripts/DocSearch.dll/BBL" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/header/Referer
-echo
 curl -X POST -d "value=50" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/get/max_rows
-echo
 curl -X POST -d "value=To Current Date" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_selectdate
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_datefromm
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_datefromd
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_datefromy
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_datetom
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_datetod
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_datetoy
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_doctype_name
-echo
 curl -X POST -d "value=50" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_max_rows
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_page
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_ReqID
-echo
 curl -X POST -d "value=N" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_ISIntranet
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_EmployeeID
-echo
 curl -X POST -d "value=BBL" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_SearchType
-echo
 curl -X POST -d "value=YES" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/cookie/JUMPPAGE
-echo
 curl -X POST -d "value=MORTGAGES & INSTRUMENTS" $URL/gatherer/NY.NYC.ACRIS.Parcel.Mortgages/post/hid_doctype_name
-echo
 curl -X POST -d "value=ALL_MORT" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_doctype
-echo
 curl -X POST -d "value=DEEDS AND OTHER CONVEYANCES" $URL/gatherer/NY.NYC.ACRIS.Parcel.Mortgages/post/hid_doctype_name
-echo
 curl -X POST -d "value=ALL_DEED" $URL/gatherer/NY.NYC.ACRIS.Parcel.All/post/hid_doctype
-echo
 curl -X POST -d "value=http://a810-bisweb.nyc.gov/bisweb/bispi00.jsp" $URL/gatherer/NY.NYC.BIS.Profile/header/Referer
-echo
 curl -X POST -d "value= GO " $URL/gatherer/NY.NYC.BIS.Profile/get/go2
-echo
 curl -X POST -d "value=0" $URL/gatherer/NY.NYC.BIS.Profile/get/requestid
-echo
 curl -X POST -d "value=$R{boroughNumber}" $URL/gatherer/NY.NYC.BIS.Profile/get/boro
-echo
 curl -X POST -d "value=$R{streetNumber}" $URL/gatherer/NY.NYC.BIS.Profile/get/houseno
-echo
 curl -X POST -d "value=$R{streetDir} $R{streetName} $R{streetSuffix}" $URL/gatherer/NY.NYC.BIS.Profile/get/street
-echo
 curl -X POST -d "value=http://webapps.nyc.gov:8084/CICS/fin1/find001I" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/header/Referer
-echo
 curl -X POST -d "value=" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/post/FAPTNUM
-echo
 curl -X POST -d "value=A" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/post/FFUNC
-echo
 curl -X POST -d "value=SEARCH" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/post/DFH_ENTER
-echo
 curl -X POST -d "value=$R{boroughNumber}" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/post/FBORO
-echo
 curl -X POST -d "value=$R{streetNumber}" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/post/FHOUSENUM
-echo
 curl -X POST -d "value=$R{streetDir} $R{streetName} $R{streetSuffix}" $URL/gatherer/NY.NYC.DOF.PropertyAddressSearch/post/FSTNAME
-echo
 
-echo
+echo "Creating Informations:"
 curl -X POST $URL/information/NY.NYC/Property/gatherer/NY.NYC.BIS.Profile
-echo
 curl -X POST $URL/information/NY.NYC/Property/gatherer/NY.NYC.DOF.PropertyAddressSearch
-echo
 curl -X POST $URL/information/NY.NYC/Property/gatherer/NY.NYC.ACRIS.Parcel.Deeds
-echo
 curl -X POST $URL/information/NY.NYC/Property/gatherer/NY.NYC.ACRIS.Parcel.Mortgages
-echo
 
-echo
+echo "Creating Interpreters:"
 curl -X POST -d "regex=<!--Table Begin!-->([\s\S]+?)<!--Table End-->" $URL/information/NY.NYC/Property/gatherer.NY.NYC.ACRIS.Parcel.Deeds/0/to/ACRIS.Parcel.Deeds.table
-echo
 curl -X POST -d "regex=<!--Table Begin!-->([\s\S]+?)<!--Table End-->" $URL/information/NY.NYC/Property/gatherer.NY.NYC.ACRIS.Parcel.Mortgages/0/to/ACRIS.Parcel.Mortgages.table
-echo
 curl -X POST -d "regex=BIN#[^\d*](\d+)" $URL/information/NY.NYC/Property/gatherer.NY.NYC.BIS.Profile/0/to/BIN
-echo
 curl -X POST -d "regex=BIN#[^\d*](\d+)" $URL/information/NY.NYC/Property/gatherer.NY.NYC.BIS.Profile/0/to/BIN
-echo
 curl -X POST -d "regex=<td class=\"maininfo\"[^>]*>[^<]*</td>[^<]*<td class=\"maininfo\"[^>]*>([^0-9]+)" $URL/information/NY.NYC/Property/gatherer.NY.NYC.BIS.Profile/0/to/BIS.city
-echo
 curl -X POST -d "<input\s+type=\"hidden\"\s+name=\"q49_block_id\"\s+value=\"([^\"]+)\">" $URL/information/NY.NYC/Property/gatherer.NY.NYC.DOF.PropertyAddressSearch/0/to/block
-echo
 curl -X POST -d "<input\s+type=\"hidden\"\s+name=\"q49_lot\"\s+value=\"([^\"]+)\">" $URL/information/NY.NYC/Property/gatherer.NY.NYC.DOF.PropertyAddressSearch/0/to/lot
-echo
 
-echo
 curl -X POST -d "<input\s+type=\"hidden\"\s+name=\"ownerName\d{0,1}\"\s+value=\"\s*([^\"]+)\">" $URL/information/NY.NYC/Property/gatherer.NY.NYC.DOF.PropertyAddressSearch/to/NY.NYC/Owner/name
-echo
