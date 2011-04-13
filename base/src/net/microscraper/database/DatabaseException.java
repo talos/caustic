a23084cc4e126b1f7b4612c33c330b43e0532b9c
Talos <irving.krauss@gmail.com>
2011-04-13 18:24:10 -0400

    deleting extraneous files

--- base/src/net/microscraper/database/DatabaseException.java
+++ base/src/net/microscraper/database/DatabaseException.java
@@ -6,15 +6,7 @@ public class DatabaseException extends Exception {
 	 */
 	private static final long serialVersionUID = -4737299738008794427L;
 	public DatabaseException(String message) { super(message); }
-	/*
-	public static class ModelNotFoundException extends DatabaseException {
-
-		private static final long serialVersionUID = 4961650071060423898L;
-		public ModelNotFoundException(String model_key) {
-			super("Could not find model '" + model_key + "'."); 
-		}
-	}
-	*/
+	
 	public static class ResourceNotFoundException extends DatabaseException {
 
 		/**
