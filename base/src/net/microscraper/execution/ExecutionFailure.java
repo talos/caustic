485224f08178b512a7aa77a8bc35abc5c1199d08
talos <irving.krauss@gmail.com>
2011-05-21 15:23:11 -0400

    documentation

---
+++ base/src/net/microscraper/execution/ExecutionFailure.java
@@ -0,0 +1,24 @@
+package net.microscraper.execution;
+
+/**
+ * An {@link Exception} that indicates an {@link Execution}
+ * has failed.  Its {@link #getCause} is the reason
+ * for the failure.
+ * @author john
+ *
+ */
+public class ExecutionFailure extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9180369308508105666L;
+	private final Exception failure;
+	public ExecutionFailure(Exception failure) {
+		super(failure);
+		this.failure = failure;
+	}
+	
+	public Throwable getCause() {
+		return failure;
+	}
+}
