3c054203cafe3812934ed79a424506f61f2d809c
talos <irving.krauss@gmail.com>
2011-08-19 22:12:55 -0400

    fixed eclipse's erasure of JSONParser and JSONIterator

---
+++ base/src/net/microscraper/json/JSONParser.java
@@ -0,0 +1,66 @@
+package net.microscraper.json;
+
+import java.util.Hashtable;
+
+import net.microscraper.uri.Uri;
+import net.microscraper.uri.MalformedUriException;;
+
+/**
+ * Implementations provide a fully-featured interface for microscraper to
+ * handle JSON with references.  The format
+ * of this interface is indebted to org.json.me, but also should implement
+ * JSON referencing when the object is first initialized.
+ * @author john
+ * @see #REFERENCE_KEY
+ * @see #EXTENDS
+ *
+ */
+public interface JsonParser {
+	/**
+	 * When the parser encounters this as a key in an object, it should replace
+	 * the object with the contents of the JSON loaded from the URI that is this
+	 * key's value.
+	 */
+	public static final String REFERENCE_KEY = "$ref";
+	
+	/**
+	 * When the parser encounters this as a key in an object, it should append 
+	 * the key-value pairs of the value object into the containing object.  If
+	 * the value is an array, it should append all of the key-value pairs of
+	 * each array element into the original object.
+	 */
+	public static final String EXTENDS = "extends";
+	
+	/**
+	 * Load a {@link JsonObject} from a {@link Uri}.
+	 * @param location The {@link Uri} URI to load.
+	 * @return A {@link JsonObject}.
+	 * @throws JsonException If there is an error generating
+	 * the {@link JsonObject}.
+	 * @throws MalformedUriException if the {@link Uri} could not be resolved.
+	 */
+	public abstract JsonObject load(Uri location) 
+			throws JsonException, MalformedUriException;
+	
+	/**
+	 * Compile a {@link JsonObject} directly from a {@link String}.
+	 * @param location The {@link Uri} URI to use when resolving <code>jsonString</code>'s references.
+	 * @param jsonString The {@link String} to parse.
+	 * @return A {@link JsonObject}.
+	 * @throws JsonException If there is an error generating
+	 * the {@link JsonObject}.
+	 * @throws MalformedUriException if the {@link Uri} could not be resolved.
+	 */
+	public abstract JsonObject parse(Uri location, String jsonString)
+			throws JsonException, MalformedUriException;
+	
+	/**
+	 * Compile a flat {@link JsonObject} from a {@link Hashtable} of
+	 * {@link String} to {@link String} mappings.
+	 * @param map A {@link Hashtable} of {@link String} to {@link String} mappings.
+	 * @return A flat {@link JsonObject} with the same mappings as <code>hash</code>.
+	 * @throws JsonException if <code>hash</code> could not be converted into a flat
+	 * {@link JsonObject}.
+	 */
+	public abstract JsonObject generate(Hashtable map) throws JsonException;
+}
