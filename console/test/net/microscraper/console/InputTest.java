4c56b898c7ba51e1986d69d8e6e89e86661e2705
talos <irving.krauss@gmail.com>
2011-09-10 13:30:58 -0400

    fixing up tests for console

--- console/test/net/microscraper/console/InputTest.java
+++ console/test/net/microscraper/console/InputTest.java
@@ -5,7 +5,12 @@ import static net.microscraper.util.TestUtils.*;
 
 import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Map;
+
+import net.microscraper.database.Database;
+import net.microscraper.database.DatabaseView;
+import net.microscraper.database.HashtableDatabase;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +22,7 @@ public class InputTest {
 	public static final String PATH_TO_BBLS = PATH_TO_CSVS + "bbls.csv";
 	
 	private Hashtable<String, String> shared;
+	private Database database;
 
 	@Before
 	public void setUp() throws Exception {
@@ -24,17 +30,19 @@ public class InputTest {
 		for(int i = 0 ; i < 4 ; i ++) {
 			shared.put(randomString(), randomString());
 		}
+		database = new HashtableDatabase();
 	}
-
+	
 	@Test
 	public void testFromSharedAndCSVCombinesMaps() throws Exception {
 		Input input = Input.fromSharedAndCSV(shared, PATH_TO_QUERIES, ',');
 		input.open();
-		Map<String, String> map;
-		while((map = input.next()) != null) {
-			assertTrue(map.keySet().containsAll(shared.keySet()));
-			assertTrue(map.values().containsAll(shared.values()));
-			assertTrue(map.size() > shared.size());
+		DatabaseView view;
+		while((view = input.next(database)) != null) {
+			for(String key : shared.keySet()) {
+				assertEquals(shared.get(key), view.get(key));
+			}
+			assertNotNull(shared.get("query"));
 		}
 	}
 
@@ -43,8 +51,8 @@ public class InputTest {
 		Hashtable<String, String> shared = new Hashtable<String, String>();
 		Input input = Input.fromShared(shared);
 		input.open();
-		input.next();
-		assertNull(input.next());
+		input.next(database);
+		assertNull(input.next(database));
 		input.close();
 	}
 	
@@ -54,13 +62,13 @@ public class InputTest {
 		input.open();
 		Map<String, String> row;
 		
-		row = input.next();
+		row = input.next(database);
 		assertTrue(row.keySet().containsAll(Arrays.asList("Borough", "Block", "Lot")));
 		assertEquals("3", row.get("Borough"));
 		assertEquals("1772", row.get("Block"));
 		assertEquals("74", row.get("Lot"));
 
-		row = input.next();
+		row = input.next(database);
 		assertTrue(row.keySet().containsAll(Arrays.asList("Borough", "Block", "Lot")));
 		assertEquals("1", row.get("Borough"));
 		assertEquals("1171", row.get("Block"));
